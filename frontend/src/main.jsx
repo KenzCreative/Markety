c54690998d9eff320e1daa808c99f3b0ceed44aa
Kenz Creative <mackenzieoliver97@gmail.com>
2025-04-02 21:20:04 -0500

    Fresh new working index.html

---
+++ frontend/src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './pages/styles/index.jsx'; // this is your real app
+import './styles/globals.css'; // if you have global CSS
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
